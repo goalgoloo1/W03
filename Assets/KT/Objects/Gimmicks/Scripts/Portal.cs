dee2dd722cb181dfea250311e603d9899027604b
kyeongtae-git <choikt9012@gmail.com>
2025-03-21 15:55:10 +0900

    [Feat, Fix] Portal Block 추가, 스크립트 직렬화
    1. Portal Block 추가
    2. Auto Move Block 직렬화
    3. Movable Block 직렬화
    4. Breakable Block 직렬화
    5. Fallable Block 직렬화

---
+++ Assets/KT/Objects/Gimmicks/Scripts/Portal.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] Transform _exit; //출구
+
+    private void Start()
+    {
+        _exit = transform.parent.GetComponentInChildren<PortalExit>().GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Teleport(collider);
+    }
+
+    void Teleport(Collider2D collider)
+    {
+        //위치 이동
+        collider.transform.position = _exit.position;
+
+        //힘 방향 조정 (벡터 회전)
+        float portalDeg = _exit.eulerAngles.z - transform.eulerAngles.z;
+        float rad = portalDeg * Mathf.Deg2Rad;
+        float cos = Mathf.Cos(rad);
+        float sin = Mathf.Sin(rad);
+
+        Rigidbody2D collRigid = collider.GetComponent<Rigidbody2D>();
+        Vector2 collVelocity = collRigid.linearVelocity;
+
+        float x = collVelocity.x * cos - collVelocity.y * sin;
+        float y = collVelocity.x * sin + collVelocity.y * cos;
+
+        collRigid.linearVelocity = new Vector2(x, y);
+    }
+}
