1a102afbb6793945a9813b7491236f6c18a3daf4
kimHanSeong <goalgoloo1@gmail.com>
2025-03-21 14:55:14 +0900

    [Feat] Three Buttons on StageClearMenu animation

--- Assets/HS/Scripts/StageClearMenu.cs
+++ Assets/HS/Scripts/StageClearMenu.cs
@@ -4,10 +4,11 @@ using System.Threading.Tasks;
 using TMPro;
 using System;
 using Enums;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageClearMenu : MonoBehaviour
 {
-    // When player clear stage, start this event
     public Action OnStageClearMenuEvent;
 
     CanvasGroup _canvasGroup;
@@ -16,21 +17,29 @@ public class StageClearMenu : MonoBehaviour
     TextMeshProUGUI _textScore;
     TextMeshProUGUI _textClearTime;
 
+    Button _returnToMainSceneBtn;
+    Button _retryBtn;
+    Button _playNextStageBtn;
+
     float _introPosX = 500; //px
-    float _outroPosX = 1210; //px
+    float _outroPosX = 1210; //PS: not used because no outro now.
     float _tweenDuration = 0.3f;
+    //HS->DE: I added this line to get the selected stage number. 
+    int _selectedStageNum = GameManager.Instance.SelectedStageNum; 
+
+
+
 
     private void Start()
     {
 
         Debug.Log("{GameManager.Instance.StageManager}");
+        //StageClearSequence(); PS: for Debugging
         GameManager.Instance.StageManager.OnStageClearEvent += StageClearSequence;
-        // OnStageClearMenuEvent += StageClearSequence;
-        // OnStageClearMenuEvent?.Invoke();
-
     }
+
     
-    private async void StageClearSequence(RankType type, float time, int coinCount)
+    private async void StageClearSequence(RankType type, float time, int coinCount)//RankType type, float time, int coinCount
     {
         FindUIObjects();
         ChangeClearUIText(type, time, coinCount);
@@ -51,10 +60,7 @@ public class StageClearMenu : MonoBehaviour
 
         DisplayRank();
 
-        await Task.Delay(1800);
-
-        HideStageClearPanel();
-
+        DisplayButtons();
     }
 
     private void ChangeClearUIText(RankType type, float time, int coinCount)
@@ -77,6 +83,36 @@ public class StageClearMenu : MonoBehaviour
 
         _textClearTime = GameObject.Find("ClearTime").GetComponent<TextMeshProUGUI>();
         _textClearTime.gameObject.SetActive(false);
+
+        //button
+        _returnToMainSceneBtn = GameObject.Find("ReturnToMainSceneBtn").GetComponent<Button>();
+        _returnToMainSceneBtn.gameObject.SetActive(false);
+
+        _playNextStageBtn = GameObject.Find("PlayNextStageBtn").GetComponent<Button>();
+        _playNextStageBtn.gameObject.SetActive(false);
+
+        _retryBtn = GameObject.Find("RetryBtn").GetComponent<Button>();
+        _retryBtn.gameObject.SetActive(false);
+
+        _returnToMainSceneBtn.onClick.AddListener(ReturnToMainSceneBtnClick);
+        _playNextStageBtn.onClick.AddListener(PlayNextStageBtnClick);
+        _retryBtn.onClick.AddListener(RetryBtnClick);
+    }
+
+    private void ReturnToMainSceneBtnClick()
+    {
+        //HS->DE: I didn't know what does MainScene mean, so Please change this code to the correct scene.  
+        //HS->DE: MainScene이 6개 중 무엇을 의미하는지 모르겠습니다. 이 코드를 올바른 씬으로 변경부탁드립니다.
+        //GameManager.Instance.OnSelectStageEvent?.Invoke(0);
+        print("[HS->DE] Please change this code to the correct scene");
+    }
+    private void PlayNextStageBtnClick()
+    {
+        GameManager.Instance.OnSelectStageEvent?.Invoke(_selectedStageNum+1);
+    }
+    private void RetryBtnClick()
+    {
+        GameManager.Instance.OnSelectStageEvent?.Invoke(_selectedStageNum);
     }
     private void ShowStageClearPanel()
     {
@@ -84,15 +120,16 @@ public class StageClearMenu : MonoBehaviour
         _clearPanelRect.DOAnchorPosX(_introPosX, _tweenDuration).SetUpdate(true);
     }
 
-    private void HideStageClearPanel()
-    {
-        _canvasGroup.DOFade(0, _tweenDuration).SetUpdate(true);
-        _clearPanelRect.DOAnchorPosX(_outroPosX, _tweenDuration).SetUpdate(true);
+    //commented out because it's not used
+    //private void HideStageClearPanel()
+    //{
+    //    _canvasGroup.DOFade(0, _tweenDuration).SetUpdate(true);
+    //    _clearPanelRect.DOAnchorPosX(_outroPosX, _tweenDuration).SetUpdate(true);
 
-        _textRank.gameObject.SetActive(false);
-        _textClearTime.gameObject.SetActive(false);
-        _textScore.gameObject.SetActive(false);
-    }
+    //    _textRank.gameObject.SetActive(false);
+    //    _textClearTime.gameObject.SetActive(false);
+    //    _textScore.gameObject.SetActive(false);
+    //}
     private void DisplayClearTime()
     {
         _textClearTime.gameObject.SetActive(true);
@@ -106,6 +143,14 @@ public class StageClearMenu : MonoBehaviour
     private void DisplayRank()
     {
         _textRank.gameObject.SetActive(true);
+    }
+
+    private void DisplayButtons()
+    {
+        _returnToMainSceneBtn.gameObject.SetActive(true);
+        _retryBtn.gameObject.SetActive(true);
+        _playNextStageBtn.gameObject.SetActive(true);
+
     }
 
 }
